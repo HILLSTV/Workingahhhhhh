3e6f05cb66786c927a7bddcf2e0ced1e935e1e76
PJ <126437129+HILLSTV@users.noreply.github.com>
2023-03-31 17:02:09 -0400

    Add files via upload

---
+++ yes.js
@@ -0,0 +1,13 @@
+var box = document.getElementById("box");
+var yes = document.getElementById("yes");
+var no = document.getElementById("no");
+
+yes.onclick = function(){
+document.getElementById("p1").innerHTML = "awwww twanks"
+document.getElementById("p2").innerHTML = " ";
+}
+
+no.onclick = function(){
+document.getElementById("p2").innerHTML = "i hate u"
+document.getElementById("p1").innerHTML = " ";
+}
